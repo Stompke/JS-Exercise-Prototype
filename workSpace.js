319c81104e33dd45d7cf7635bff5b8b56297532b
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2019-11-13 15:08:12 -0800

    completed Car constructor challenge

--- workSpace.js
+++ workSpace.js
@@ -1,52 +1,28 @@
 /*
-  TASK 1
-    ✅- Write a Person Constructor that initializes `name` and `age` from arguments.
-    ✅- All instances of Person should initialize with an empty `stomach` array.
-    - Give instances of Person the ability to `.eat("someFood")`:
-        ✅+ When eating an edible, it should be pushed into the `stomach`.
-        ✅+ The `eat` method should have no effect if there are 10 items in the `stomach`.
-    - Give instances of Person the ability to `.poop()`:
-        ✅+ When an instance poops, its `stomach` should empty.
-    - Give instances of Person a method `.toString()`:
-        + It should return a string with `name` and `age`. Example: "Mary, 50"
+  TASK 2
+    ✅- Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+    ✅- All instances built with Car:
+        ✅+ should initialize with an `tank` at 0
+        ✅+ should initialize with an `odometer` at 0
+    ✅- Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
+        + Should cause the `odometer` to go up.
+        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+    - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
+        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Person(args) {
-  this.name = args.name,
-  this.age = args.age,
-  this.stomach = []
+function Car(model, milesPerGallon) {
+  this.model = model,
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0;
+  this.odometer = 0;
+};
+
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
 }
 
-Person.prototype.eat = function (food){
-  console.log(`${this.name} just ate some ${food}`);
-  if (this.stomach.length < 10){
-    this.stomach.push(`${food}`);
-    console.log(this.stomach);
-  } else {
-    console.log('im too full 🤢')
-  }
-}
+let truck = new Car('dodge', 16);
 
-Person.prototype.poop = function (){
-  console.log(`${this.name} just pooped 💩`);
-  this.stomach = [];
-}
-
-Person.prototype.toString = function (){
-  console.log(`${this.name}, ${this.age}`);
-  return `${this.name}, ${this.age}`;
-}
-
-
-let shawn = new Person({name: 'Shawn', age: 26});
-
-shawn.eat('pumpkins');
-shawn.eat('tomatoes');
-shawn.eat('pumpkins');
-shawn.eat('tomatoes');
-shawn.eat('pumpkins');
-shawn.eat('tomatoes');
-shawn.eat('pumpkins');
-shawn.eat('tomatoes');
-
-
+console.log(truck);
